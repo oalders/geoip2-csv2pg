139a3a4f4bf423969df3fa508bc35b39e8689389
Olaf Alders <olaf@wundersolutions.com>
2023-04-12 14:17:14 -0400

    Try to download multiple database products

--- bin/download-db.sh
+++ bin/download-db.sh
@@ -2,41 +2,46 @@
 
 set -eux -o pipefail
 
+DATABASES=("GeoIP2-Anonymous-IP" "GeoLite2-City" "GeoIP2-Enterprise")
 SHARE_DIR=share
-URL="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City-CSV&license_key=${MM_LICENSE_KEY}&suffix=zip"
-ARCHIVE_NAME=GeoLite2-City-CSV.zip
-SHA_SUFFIX=".sha256"
-
 mkdir -p $SHARE_DIR
-
 pushd $SHARE_DIR || exit 1
 
-FILE_OR_TIME=$ARCHIVE_NAME
-SHA_FILE_OR_TIME="${ARCHIVE_NAME}${SHA_SUFFIX}"
+for I in "${!DATABASES[@]}"; do
+    PRODUCT=${DATABASES[$I]}
+    URL="https://download.maxmind.com/app/geoip_download?edition_id=${PRODUCT}-CSV&license_key=${MM_LICENSE_KEY}&suffix=zip"
+    ARCHIVE_NAME="${PRODUCT}-CSV.zip"
+    SHA_SUFFIX=".sha256"
 
-# -z expects a file name or a timestamp. Let's set a timestamp far in the past,
-# so that we don't get a warning if the file does not already exist.
-if [[ ! -e "$ARCHIVE_NAME" ]];then
-    FILE_OR_TIME="1999-12-31T23:59:59"
-    SHA_FILE_OR_TIME=$FILE_OR_TIME
-fi
+    FILE_OR_TIME=$ARCHIVE_NAME
+    SHA_FILE_OR_TIME="${ARCHIVE_NAME}${SHA_SUFFIX}"
 
-curl --location -z "$FILE_OR_TIME" -v "${URL}" -o "$ARCHIVE_NAME"
-curl --location -z "$SHA_FILE_OR_TIME" -v "${URL}${SHA_SUFFIX}" -o "${ARCHIVE_NAME}${SHA_SUFFIX}"
+    # -z expects a file name or a timestamp. Let's set a timestamp far in the past,
+    # so that we don't get a warning if the file does not already exist.
+    if [[ ! -e "$ARCHIVE_NAME" ]]; then
+        FILE_OR_TIME="1999-12-31T23:59:59"
+        SHA_FILE_OR_TIME=$FILE_OR_TIME
+    fi
 
-sha256sum GeoLite2-City-CSV.zip > downloaded.sha256
+    curl --location -z "$FILE_OR_TIME" -v "${URL}" -o "$ARCHIVE_NAME"
+    curl --location -z "$SHA_FILE_OR_TIME" -v "${URL}${SHA_SUFFIX}" -o "${ARCHIVE_NAME}${SHA_SUFFIX}"
 
-# Fix up filename in downloaded SHA so that the check doesn't choke on it
-perl -pi -e 's/_\d+//g' GeoLite2-City-CSV.zip.sha256
-sha256sum --check --status downloaded.sha256 GeoLite2-City-CSV.zip.sha256
+    SHA_FILE="${ARCHIVE_NAME}.downloaded.sha256"
+    sha256sum "$ARCHIVE_NAME" >"$SHA_FILE"
 
-DBS=dbs
-mkdir -p "$DBS"
+    # Fix up filename in downloaded SHA so that the check doesn't choke on it
+    perl -pi -e 's/_\d+//g' "${ARCHIVE_NAME}.sha256"
+    ls
+    sha256sum --check --status "$SHA_FILE" "${ARCHIVE_NAME}.sha256"
 
-TMPDIR=$(mktemp -d /tmp/mmcsv.XXXXXXXXXX) || exit 1
+    DBS="dbs/${PRODUCT}"
+    mkdir -p "$DBS"
 
-unzip "$ARCHIVE_NAME" -d "$TMPDIR"
-find "$TMPDIR" -type f -name '*Blocks*' -print0 | xargs -0 -I '{}' cp '{}' "${DBS}/"
-find "$TMPDIR" -type f -name 'GeoLite2-City-Locations-en.csv' -print0 | xargs -0 -I '{}' cp '{}' "${DBS}/"
+    TMPDIR=$(mktemp -d /tmp/mmcsv.XXXXXXXXXX) || exit 1
+
+    unzip "$ARCHIVE_NAME" -d "$TMPDIR"
+    find "$TMPDIR" -type f -name '*Blocks*' -print0 | xargs -0 -I '{}' cp '{}' "${DBS}/"
+    find "$TMPDIR" -type f -name "${PRODUCT}-Locations-en.csv" -print0 | xargs -0 -I '{}' cp '{}' "${DBS}/"
+done
 
 exit 0
--- schema.sql
+++ schema.sql
@@ -1,48 +1,57 @@
-create table geoip2_network (
-  network cidr not null,
-  geoname_id int,
-  registered_country_geoname_id int,
-  represented_country_geoname_id int,
-  is_anonymous_proxy bool,
-  is_satellite_provider bool,
-  postal_code text,
-  latitude numeric,
-  longitude numeric,
-  accuracy_radius int
-);
+do $$
+declare
+    file_found BOOLEAN;
+begin
+    SELECT exists (select size from pg_stat_file('/share/dbs/GeoLite2-City/GeoLite2-City-Blocks-IPv4.csv', true) ) INTO file_found;
+    if file_found then
+        create table geoip2_network (
+          network cidr not null,
+          geoname_id int,
+          registered_country_geoname_id int,
+          represented_country_geoname_id int,
+          is_anonymous_proxy bool,
+          is_satellite_provider bool,
+          postal_code text,
+          latitude numeric,
+          longitude numeric,
+          accuracy_radius int
+        );
 
-create index on geoip2_network using gist (network inet_ops);
+        create index on geoip2_network using gist (network inet_ops);
 
-create table geoip2_location (
-  geoname_id int not null,
-  locale_code text not null,
-  continent_code text not null,
-  continent_name text not null,
-  country_iso_code text,
-  country_name text,
-  subdivision_1_iso_code text,
-  subdivision_1_name text,
-  subdivision_2_iso_code text,
-  subdivision_2_name text,
-  city_name text,
-  metro_code int,
-  time_zone text,
-  is_in_european_union bool not null,
-  primary key (geoname_id, locale_code)
-);
+        create table geoip2_location (
+          geoname_id int not null,
+          locale_code text not null,
+          continent_code text not null,
+          continent_name text not null,
+          country_iso_code text,
+          country_name text,
+          subdivision_1_iso_code text,
+          subdivision_1_name text,
+          subdivision_2_iso_code text,
+          subdivision_2_name text,
+          city_name text,
+          metro_code int,
+          time_zone text,
+          is_in_european_union bool not null,
+          primary key (geoname_id, locale_code)
+        );
 
-copy geoip2_network(
-  network, geoname_id, registered_country_geoname_id, represented_country_geoname_id,
-  is_anonymous_proxy, is_satellite_provider, postal_code, latitude, longitude, accuracy_radius
-) from '/share/dbs/GeoLite2-City-Blocks-IPv4.csv' with (format csv, header);
+        copy geoip2_network(
+          network, geoname_id, registered_country_geoname_id, represented_country_geoname_id,
+          is_anonymous_proxy, is_satellite_provider, postal_code, latitude, longitude, accuracy_radius
+        ) from '/share/dbs/GeoLite2-City/GeoLite2-City-Blocks-IPv4.csv' with (format csv, header);
 
-copy geoip2_network(
-  network, geoname_id, registered_country_geoname_id, represented_country_geoname_id,
-  is_anonymous_proxy, is_satellite_provider, postal_code, latitude, longitude, accuracy_radius
-) from '/share/dbs/GeoLite2-City-Blocks-IPv6.csv' with (format csv, header);
+        copy geoip2_network(
+          network, geoname_id, registered_country_geoname_id, represented_country_geoname_id,
+          is_anonymous_proxy, is_satellite_provider, postal_code, latitude, longitude, accuracy_radius
+        ) from '/share/dbs/GeoLite2-City/GeoLite2-City-Blocks-IPv6.csv' with (format csv, header);
 
-copy geoip2_location(
-  geoname_id, locale_code, continent_code, continent_name, country_iso_code, country_name,
-  subdivision_1_iso_code, subdivision_1_name, subdivision_2_iso_code, subdivision_2_name,
-  city_name, metro_code, time_zone, is_in_european_union
-) from '/share/dbs/GeoLite2-City-Locations-en.csv' with (format csv, header);
+        copy geoip2_location(
+          geoname_id, locale_code, continent_code, continent_name, country_iso_code, country_name,
+          subdivision_1_iso_code, subdivision_1_name, subdivision_2_iso_code, subdivision_2_name,
+          city_name, metro_code, time_zone, is_in_european_union
+        ) from '/share/dbs/GeoLite2-City/GeoLite2-City-Locations-en.csv' with (format csv, header);
+
+    end if;
+end $$
