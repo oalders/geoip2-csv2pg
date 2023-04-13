0ed4bad2e09c337a546c81f31d37805fea9bbd10
Olaf Alders <olaf@wundersolutions.com>
2023-04-13 10:55:40 -0400

    Set ON_ERROR_STOP when executing SQL

--- schema.sh
+++ schema.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # create tables
-psql -d mm_database -U root -a --file=/share/sql/schema.sql
+psql -d mm_database -U root -a -v "ON_ERROR_STOP=1" --file=/share/sql/schema.sql
 
 SHARE=/share/dbs
 pushd $SHARE || exit 1
@@ -14,12 +14,12 @@ for PRODUCT in "${PRODUCTS[@]}"; do
     echo "🎸 starting product $PRODUCT"
     FILE_PREFIX="${SHARE}/${PRODUCT}/${PRODUCT}"
 
-    psql -d mm_database -U root -a -v file="${FILE_PREFIX}-Blocks-IPv4.csv" -f /share/sql/copy_network.sql
-    psql -d mm_database -U root -a -v file="${FILE_PREFIX}-Blocks-IPv6.csv" -f /share/sql/copy_network.sql
+    psql -d mm_database -U root -a -v "ON_ERROR_STOP=1" -v file="${FILE_PREFIX}-Blocks-IPv4.csv" -f /share/sql/copy_network.sql
+    psql -d mm_database -U root -a -v "ON_ERROR_STOP=1" -v file="${FILE_PREFIX}-Blocks-IPv6.csv" -f /share/sql/copy_network.sql
 
     LOCATIONS_FILE="${FILE_PREFIX}-Locations-en.csv"
     if [[ -f $LOCATIONS_FILE ]]; then
-        psql -d mm_database -U root -a -v file="$LOCATIONS_FILE" -f /share/sql/copy_location.sql
+        psql -d mm_database -U root -a -v "ON_ERROR_STOP=1" -v file="$LOCATIONS_FILE" -f /share/sql/copy_location.sql
     fi
 
 done
