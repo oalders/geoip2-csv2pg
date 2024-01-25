0ef78ca6751baca5e1709cdcfa1f3c20f922a46b
Christopher Luna <christophermluna@gmail.com>
2024-01-25 12:02:14 -0500

    Remove unnecessary flag

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-docker run -e POSTGRES_PASSWORD=foo --rm --shm-size=1g -p 5432:5432 --volume "$PWD/share/dbs:/share/dbs" mmpg
+docker run -e POSTGRES_PASSWORD=foo --rm -p 5432:5432 --volume "$PWD/share/dbs:/share/dbs" mmpg
